2f5775a767acc1ba41bb4461bc02ac648d0c9dc2
l-trap <luigi.trapassi@gmail.com>
2013-03-06 15:06:56 +0100

    exercise01 correct1

--- learning-javascript/2013-03-05/exercise01.js
+++ learning-javascript/2013-03-05/exercise01.js
@@ -1,8 +1,8 @@
 var table = "";
 for(var i=1; i<=10; i++){
 	for(var j=1; j<=10; j++){
-		if(j<10) line += (i*j + "\t");
-		else 	 line += (i*j + "\n"); 	
+		if(j<10) table += (i*j + "\t");
+		else 	 table += (i*j + "\n"); 	
 	}
 }
 console.log(table);
