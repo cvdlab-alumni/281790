4dd2bfb71f6a5c3354968f1d4ab20bb048f48244
l-trap <luigi.trapassi@gmail.com>
2013-04-06 17:53:52 +0200

    bug fix

--- 2013-04-05/python/exercise1.py
+++ 2013-04-05/python/exercise1.py
@@ -2,6 +2,8 @@
 # Define, with names pillars0, pillars1, pillars2, and pillars3, the models of pillars of the 4 house floors, 
 # and put them into the STRUCT of an initial building model.
 
+GRID = COMP([INSR(PROD),AA(QUOTE)])
+
 # PILLARS
 
 # pillar
