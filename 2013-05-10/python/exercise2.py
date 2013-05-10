022b9e96ca64d3c2b0d4cadb70a10f83c21a789f
l-trap <luigi.trapassi@gmail.com>
2013-05-11 01:56:04 +0200

    front

--- 2013-05-10/python/exercise2.py
+++ 2013-05-10/python/exercise2.py
@@ -64,31 +64,78 @@ def profile_xz():
 	return side
 
 
-# VIEW(profile_xz())
+VIEW(profile_xz())
 
 
 #profile with x=0
 
 def profile_yz():
-	#control points
+	#generic control points
 	p0 = [[0,0.7,0.07],[0,1.06,0.14],[0,1.57,0.16],[0,2.1,0.25]]
-	p1 = [[0,2.1,0.25],[0,2.57,0.27]]
+	p1 = [[0,2.1,0.25],[0,2.64,0.27]]
 	p2 = [[0,0.7,0.07],[0,0.55,0.26],[0,0.51,1],[0,0.45,1.42],[0,0.61,1.5]]
-	p3 = 
+	p3 = [[0,0.61,1.5],[0,0.83,1.6],[0,1.07,2],[0,1.2,2.3]]
+	p4 = [[0,1.2,2.3], [0,1.47,2.4],[0,1.9,2.4],[0,2.3,2.5],[0,2.6,2.5]]
 
-	#bezier curves
+	#front control points
+	#### values for cubic HERMITE
+	# fp0 = [[0,0.86,1.6],[0,1.2,1.5],[0,1.36,1.39],[0,1.6,1.15]]
+	fp0 = [[0,0.86,1.4],[0,1.6,1.15],[0,0.8,1],[0,0.3,-0.8]]
+	
+	fp1 = [[0,2.64,1.6],[0,1,1.6]]
+	fp2 = [[0,1,1.6],[0,1,1.9],[0,1.2,2.1],[0,1.4,2.3]]
+	fp3 = [[0,1.4,2.3],[0,2.1,2.3],[0,2.64,2.3]]
+
+	#faro
+	fp4 = [[0,0.7,1.3],[0,0.9,1.37],[0,1.1,1.3],[0,1.2,1.1],[0,1.2,1]]
+	fp5 = [[0,1.2,1],[0,0.9,1],[0,0.7,1.2],[0,0.7,1.3]]
+
+
+
+	# generic bezier curves
 	b0 = BEZIER(S1)(p0)
 	b1 = BEZIER(S1)(p1)
 	b2 = BEZIER(S1)(p2)
+	b3 = BEZIER(S1)(p3)
+	b4 = BEZIER(S1)(p4)
 
-	# MAPPING
+	#front bezier curves
+	fb0 = CUBICHERMITE(S1)(fp0)
+	
+	fb1 = BEZIER(S1)(fp1)
+	fb2 = BEZIER(S1)(fp2)
+	fb3 = BEZIER(S1)(fp3)
+	fb4 = BEZIER(S1)(fp4)
+	fb5 = BEZIER(S1)(fp5)
+
+	# generic MAPPINGS
 	c0 = MAP(b0)(domain)
 	c1 = MAP(b1)(domain)
 	c2 = MAP(b2)(domain)
+	c3 = MAP(b3)(domain)
+	c4 = MAP(b4)(domain)
 
-	#structuring
-	front = STRUCT([c0,c1,c2])
+	#front mappings
+	fc0 = MAP(fb0)(domain)
+	fc1 = MAP(fb1)(domain)
+	fc2 = MAP(fb2)(domain)
+	fc3 = MAP(fb3)(domain)
+	fc4 = MAP(fb4)(domain)
+	fc5 = MAP(fb5)(domain)
 
-	return front
+
+	# generic structuring
+	faro = T([2,3])([0.2,-0.1])(STRUCT([fc4,fc5]))
+	front = STRUCT([fc0,fc1,fc2,fc3,faro])
+	g0 = STRUCT([c0,c1,c2,c3,c4,front])
+	
+
+	# r_t
+	g1 = R([1,2])(PI)(g0)
+	t_g1 = T([2])([5.2])(g1)
+	gen = STRUCT([g0,t_g1])
+
+	return gen
+
 
 VIEW(profile_yz())
